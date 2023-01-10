ca5f92e11a6189d4057b5c118bc4115cdb9d130f
makaracc <makara.chhaing@gmail.com>
2023-01-10 13:00:12 +1100

    add services

---
+++ src/services/genericService.js
@@ -0,0 +1,196 @@
+import MODELS from "../models/index";
+
+/**
+ * @author Sanchit Dang
+ * @description Generic Service Template
+ */
+export default class GenericService {
+  /**
+   *
+   * @param {String} modelName Name of the Model
+   */
+  constructor(modelName) {
+    if (!this.#isModelValid(modelName)) {
+      console.error(`Invalid model name ${modelName}`);
+      throw "Invalid model name '" + modelName + "'. Terminating app...";
+    }
+
+    this.modelName = modelName;
+    this.objects = [];
+  }
+
+  /**
+   * @private
+   * @author Sanchit Dang
+   * @description Validate if models exists
+   * @param {String} modelName name of the model
+   */
+  #isModelValid(modelName) {
+    return !(
+      !modelName ||
+      0 === modelName.length ||
+      !MODELS.hasOwnProperty(modelName)
+    );
+  }
+
+  /**
+   * @author Sanchit Dang
+   * @description Update a record in DB
+   * @param {Object} criteria
+   * @param {Object} data
+   * @param {Object} options
+   * @param {Function} callback
+   */
+  updateRecord(criteria, data, options, callback) {
+    data.updatedAt = Date.now();
+    options.lean = true;
+    options.new = true;
+    MODELS[this.modelName].findOneAndUpdate(criteria, data, options, callback);
+  }
+
+  /**
+   * @author Jason Pham
+   * @description Update multiple record in DB
+   * @param {Object} criteria
+   * @param {Object | Array} data
+   * @param {Object} options
+   * @param {Function} callback
+   */
+  updateMultipleRecords(criteria, data, options, callback) {
+    data.updatedAt = Date.now();
+    options.lean = true;
+    options.new = true;
+    MODELS[this.modelName].updateMany(criteria, data, options, callback);
+  }
+
+  /**
+   * @author Sanchit Dang
+   * @description Insert a record in DB
+   * @param {Object} data
+   * @param {Function} callback
+   */
+  createRecord(data, callback) {
+    MODELS[this.modelName](data).save(callback);
+  }
+
+  /**
+   * @author Jason Pham
+   * @description Insert multiple records in DB
+   * @param {Array<Object>} data
+   * @param {Function} callback
+   */
+  createMultipleRecords(data, callback) {
+    MODELS[this.modelName].insertMany(data, callback);
+  }
+
+  /**
+   * @author Sanchit Dang
+   * @description Hard delete a record
+   * @param {Object} criteria
+   * @param {Function} callback
+   */
+  deleteRecord(criteria, callback) {
+    MODELS[this.modelName].findOneAndRemove(criteria, callback);
+  }
+
+  /**
+   * @author Sanchit Dang
+   * @description Retrive records
+   * @param {Object} criteria
+   * @param {Object} projection
+   * @param {Object} options
+   * @param {Function} callback
+   */
+  getRecord(criteria, projection, options, callback) {
+    options.lean = true;
+    MODELS[this.modelName].find(criteria, projection, options, callback);
+  }
+
+  /**
+   * @author Jason Pham
+   *
+   * @param {Object} criteria
+   * @param {Object} projection
+   * @param {Object} options
+   * @param {Object} sortOptions
+   * @param {Function} callback
+   */
+  getSortedRecord(criteria, projection, options, sortOptions, callback) {
+    options.lean = true;
+    MODELS[this.modelName]
+      .find(criteria, projection, options, callback)
+      .sort(sortOptions);
+  }
+
+  /**
+   * @author Sanchit Dang
+   * @description Retrive records while populating them
+   * @param {Object} criteria
+   * @param {Object} projection
+   * @param {Object} populate
+   * @param {Function} callback
+   */
+  getPopulatedRecords(criteria, projection, populate, callback) {
+    MODELS[this.modelName]
+      .find(criteria)
+      .select(projection)
+      .populate(populate)
+      .exec(callback);
+  }
+
+  /**
+   * @author Jason Pham
+   *
+   * @param {Object} criteria
+   * @param {Object} projection
+   * @param {Object} populate
+   * @param {Object} sortOptions
+   * @param {Function} callback
+   */
+  getSortedPopulatedRecords(
+    criteria,
+    projection,
+    populate,
+    sortOptions,
+    callback
+  ) {
+    MODELS[this.modelName]
+      .find(criteria)
+      .select(projection)
+      .populate(populate)
+      .sort(sortOptions)
+      .exec(callback);
+  }
+
+  /**
+   * @author Sanchit Dang
+   * @description Aggregate Records
+   * @param {Object} criteria
+   * @param {Function} callback
+   */
+  aggregate(criteria, callback) {
+    MODELS[this.modelName].aggregate(criteria, callback);
+  }
+
+  /**
+   * @author Sanchit Dang
+   * @description get records using promise
+   * @param {Object} criteria
+   * @param {Object} projection
+   * @param {Object} options
+   */
+  getRecordUsingPromise(criteria, projection, options) {
+    options.lean = true;
+    return new Promise((resolve, reject) => {
+      MODELS[this.modelName].find(
+        criteria,
+        projection,
+        options,
+        function (err, data) {
+          if (err) reject(err);
+          else resolve(data);
+        }
+      );
+    });
+  }
+}
--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,7 @@
+import GenericService from "./genericService";
+
+import ForgetPasswordService from "./forgetPasswordService";
 
 export default {
-
+  SchedulingService: new GenericService("schedule"),
 };
