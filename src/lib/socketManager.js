5074d87ea5db55d2f5685061a047b94bf6fd5e03
makaracc <makara.chhaing@gmail.com>
2023-01-10 12:59:50 +1100

    create chat socket

--- src/lib/socketManager.js
+++ src/lib/socketManager.js
@@ -1,39 +1,41 @@
 import Hapi from "@hapi/hapi";
 import SocketIO from "socket.io";
 /**
-* Please use socketLogger for logging in this file try to abstain from console
-* levels of logging:
-* - TRACE - ‘blue’
-* - DEBUG - ‘cyan’
-* - INFO - ‘green’
-* - WARN - ‘yellow’
-* - ERROR - ‘red’
-* - FATAL - ‘magenta’
-*/
+ * Please use socketLogger for logging in this file try to abstain from console
+ * levels of logging:
+ * - TRACE - ‘blue’
+ * - DEBUG - ‘cyan’
+ * - INFO - ‘green’
+ * - WARN - ‘yellow’
+ * - ERROR - ‘red’
+ * - FATAL - ‘magenta’
+ */
 
 class SocketManager {
-
-    /**
-     * 
-     * @param {Hapi.Server} server HAPI Server
-     */
-    connectSocket = (server) => {
-        const io = SocketIO().listen(server.listener);
-        socketLogger.info("socket server started");
-        io.on('connection', function (socket) {
-            socketLogger.info("connection established: ", socket.id);
-            socket.emit('message', {
-                message: {
-                    type: 'connection', statusCode: 200,
-                    statusMessage: 'WELCOME TO ', data: ""
-                }
-            });
-        })
-    }
+  /**
+   *
+   * @param {Hapi.Server} server HAPI Server
+   */
+  connectSocket = (server) => {
+    const io = SocketIO().listen(server.listener);
+    socketLogger.info("socket server started port: " + server.info.port);
+    io.on("connection", function (socket) {
+      socketLogger.info("connection established: ", socket.id);
+      socket.emit("message", {
+        message: {
+          type: "connection",
+          statusCode: 200,
+          statusMessage: "WELCOME TO ",
+          data: "",
+        },
+      });
+      socket.on("send-message", function (message) {
+        socketLogger.info("message: ", message);
+        socket.broadcast.emit("receive-message", message);
+      });
+    });
+  };
 }
 
 const instance = new SocketManager();
 export default instance;
-
-
-
