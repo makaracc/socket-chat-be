57d32acc834f84b7a10cf08194384e9d29b5e4a3
makaracc <makara.chhaing@gmail.com>
2023-01-16 15:26:39 +1100

    create admin and token service

--- src/services/index.js
+++ src/services/index.js
@@ -3,5 +3,8 @@ import GenericService from "./genericService";
 import ForgetPasswordService from "./forgetPasswordService";
 
 export default {
-  SchedulingService: new GenericService("schedule"),
+  SchedulingService: new GenericService("Schedule"),
+  AdminService: new GenericService("Admin"),
+  TokenService: new GenericService("Token"),
+  ForgetPasswordService,
 };
